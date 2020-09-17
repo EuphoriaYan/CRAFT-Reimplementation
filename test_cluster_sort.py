65e1c7c619cd937a90234febd015f05a322af9c0
Euphoria Yan <ysq58000@foxmail.com>
2020-09-17 08:26:04 +0800

    Update test_cluster_sort.py

--- test_cluster_sort.py
+++ test_cluster_sort.py
@@ -1,15 +1,23 @@
 
 
 from PIL import Image, ImageDraw
-img = Image.open('dataset/chinese_books/微信图片_20200719140117.jpg').convert('RGB')
-shape = img.size
 
-draw = ImageDraw.Draw(img)
-with open('output/res_微信图片_20200719140117.txt', 'r', encoding='utf-8') as fp:
-    for i, line in enumerate(fp):
-        line = line.strip()
-        if not line:
-            continue
-        rec = [int(num) for num in line.split(',')]
-        draw.rectangle(rec, outline=(255, 0, 0), width=3)
-        img.save('output/%d.jpg' % i)
+if __name__ == '__main__':
+    img = Image.open('dataset/chinese_books/微信图片_20200719140117.jpg').convert('RGB')
+    shape = img.size
+
+    draw = ImageDraw.Draw(img)
+    with open('output/res_微信图片_20200719140117.txt', 'r', encoding='utf-8') as fp:
+        for i, line in enumerate(fp):
+            line = line.strip()
+            if not line:
+                continue
+            rec = [int(num) for num in line.split(',')]
+
+            draw.rectangle(rec, outline=(255, 0, 0), width=3)
+            img.save('output/%d.jpg' % i)
+
+            '''
+            piece = img.crop(rec)
+            piece.show()
+            '''
