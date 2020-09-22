3c4fce3d2956d079f3f759bb48b0431b9a0b8109
Euphoria Yan <ysq58000@foxmail.com>
2020-09-22 11:18:17 +0800

    update

--- scripts/run_pseudodata_train.sh
+++ scripts/run_pseudodata_train.sh
@@ -2,9 +2,10 @@
 export CUDA_VISIBLE_DEVICES=1
 
 python trainPseudodata.py \
---batch_size 24 \
+--batch_size 16 \
 --lr 3.2768e-5 \
 --momentum 0.9 \
 --weight_decay 5e-4 \
 --epochs 200 \
 --test_folder dataset/chinese_books \
+--show_time \
